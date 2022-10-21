54df225416a858913ae531f6bcf2b076aad99659
valengabin <84356878+valengabin@users.noreply.github.com>
2022-10-21 19:55:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let menu = document.querySelector('#menu')
+
+let menu-bar = document.querySelector('#menu-bar')
+
+menu - bar.addEventListner('click', function () {
+    menu.classList.toggle('menu-toggle');
+
+})
