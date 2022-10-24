a9e4c56734b3f88b3878da95aba67a04672974ff
soso <avilasofia076@gmail.com>
2022-10-24 17:53:45 -0300

    Página de contacto agregada

---
+++ javaScript/index.js
@@ -0,0 +1,17 @@
+var cad=`<nav class="nav-bar">
+<div class="logo">
+    <i class="fa-solid fa-leaf"></i>    
+</div>
+<ul class="menu" id="menu">
+    <li><a href="index.html">Inicio</a></li>
+    <li><a href="#PlantasAutoctonas">Plantas Autóctonas</a></li>
+    <li><a href="#Exoticas">Exóticas</a></li>
+    <li><a href="contacto.html">Contacto</a></li>
+</ul>
+<div class="menubar" id="menubar">
+    <i class="fa-solid fa-bars"></i>
+</div>
+
+</nav>
+`
+document.getElementById('idHeader').innerHTML= cad;
