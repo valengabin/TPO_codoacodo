a1f5f2fca45840e49ee02491435ce4ddf670b17e
soso <avilasofia076@gmail.com>
2022-10-26 15:22:22 -0300

    Corrección API- Ya funciona

--- javaScript/APIClima.js
+++ javaScript/APIClima.js
@@ -1,23 +1,27 @@
-var button= document.querySelector('.button')
-var inputValue= document.querySelector('.inputValue')
-var name= document.querySelector('.name');
-var description= document.querySelector('.description');
-var temp= document.querySelector('.temp');
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
 
 button.addEventListener('click', function(name){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=797fed1193d485af22f90c544a09260d')
-    .then(response => response.json())
-    .then(data => {
-      var tempValue = data['main']['temp'];
-      var nameValue = data['name'];
-      var descValue = data['weather'][0]['description'];
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=797fed1193d485af22f90c544a09260d')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
+
     
-      main.innerHTML = nameValue;
-      desc.innerHTML = "Desc - "+descValue;
-      temp.innerHTML = "Temp - "+tempValue;
-      input.value ="";
-    
-    })
-    
-    .catch(err => alert("Wrong city name!"));
-    })
